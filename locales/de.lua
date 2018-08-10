369fe741298268ca5bc37c9c2e74b43a142769a1
NebelRebell <40825041+NebelRebell@users.noreply.github.com>
2018-08-11 01:58:49 +0200

    DE Translation
    Include de.lua - German Translation

---
+++ locales/de.lua
@@ -0,0 +1,8 @@
+Locales['de'] = {
+  ['left_instance'] 		= 'Du hast das Objekt verlassen.',
+  ['invite_expired'] 		= 'Einladung abgelaufen!',
+  ['press_to_enter'] 		= 'Benutze ~INPUT_CONTEXT~ um das Objekt zu betreten.',
+  ['entered_instance'] 		= 'Du hast das Objekt betreten',
+  ['entered_into'] 			= ' Objekt betreten',
+  ['left_out'] 				= ' Objekt verlassen',
+}
